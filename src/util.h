5a895bf71fb0441c01c618092778786b6255923e
zlogdanbr <54685402+zlogdanbr@users.noreply.github.com>
2019-12-30 17:34:45 -0300

    Create util.h
    contains useful code for search, debug, etc

---
+++ src/util.h
@@ -0,0 +1,306 @@
+//--------------------------------------------------------------------------
+//	namepsace _dutil_
+//	contains useful code for search, debug, etc
+//--------------------------------------------------------------------------
+#ifndef _UTIL_D
+#define _UTIL_D
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cassert>
+#include <list>
+#include <sstream>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+typedef multiset<long long> mset_long;
+
+namespace _dutil_ 
+{
+
+	/*
+	*	For as long as you send output to stringstream& out
+	*	this function should output it all at the file  filename
+	*/
+	inline
+	void print_all_debugs( stringstream& out, string& filename )
+	{
+		ofstream myFile;
+		myFile.open( filename, ios_base::out);
+		if (myFile.is_open())
+		{
+			myFile << out.str() << endl;
+			myFile.close();
+		}
+	}
+
+
+	/*
+	*	For as long as the container type T supports the
+	*   increment operator for the iterator this should
+	*   print the container in the stringstream out
+	*/
+	template<typename T>
+	inline 
+	void pr( const T& t, stringstream& out ) 
+	{
+		out << "[ ";
+		for( const auto& _el: t )
+		{
+			out << _el << " ";
+		}
+		out << "]\n";
+	}
+	
+	/*
+	*	For as long as the container type T supports the
+	*   increment operator for the iterator this should
+	*   print the container in the stringstream out
+	*   type O must be an output stream
+	*/
+	template<typename T, typename O>
+	inline 
+	void pr( const T& t, O& out ) 
+	{
+		out << "[ ";
+		for( const auto& _el: t )
+		{
+			out << _el << " ";
+		}
+		out << "]\n";
+	}
+	
+
+	/*
+	*	Prints a list of pairs, which is a double linked list
+	*	of pairs
+	*/
+	template<typename T, typename M>
+	inline
+	void pr( const list<pair<T,M>>& m, stringstream& out )
+	{
+		for( const auto& im: m )
+		{
+			out << "[" << im.first << "," << im.second << "]" << " ";
+		}
+		out << "\n";  
+	}
+
+	// from Bjarne Stroustrup The Pratice Of Programming
+	// optimized search function
+	template<typename In, typename T>
+	inline In _find(In first, In last, const T& val)
+	{
+		while (first!=last && *first != val) ++first;
+			return first;
+	}
+
+	// Implements a pair class of two integers
+	// it should be used mostly because of performance issues
+	class PairDerived
+	{
+	public:
+		PairDerived(int& one, int& two ):first(one),second(two){};
+		PairDerived( const PairDerived& pd)
+		{
+			this->first = pd.first;
+			this->second = pd.second;
+
+		};
+		PairDerived& operator=( const PairDerived& pd )
+		{
+			this->first = pd.first;
+			this->second = pd.second;
+			return *this;
+		};
+
+		friend bool operator==( const PairDerived& p1, const PairDerived& p2 )
+		{
+			if  ( p1.first == p2.first)
+				if ( p1.second == p2.second)
+					return true;
+			return false;   
+		}
+
+		friend bool operator!=( const PairDerived& p1, const PairDerived& p2 )
+		{
+			return ( p1.first != p2.first );
+		}
+
+		int first;
+		int second;
+	};
+
+
+	/*
+	*	Optmized search function
+	*/
+	inline mset_long::iterator _find2(mset_long& s, const ll& v )
+	{
+		mset_long::iterator it = s.begin();
+		int a = 0; 
+		int b = s.size()-1;
+		int i = 0;
+
+		while( a <= b )
+		{
+			int k = (a+b)/2;
+			for(; i < k; i++,it++);
+			if ( *it == v)
+			{
+				return it;
+			}
+
+			if ( *it > v ) 
+			{
+				b = k - 1;
+				it--;
+				i--;
+			}
+			else 
+			{
+				a = k + 1;
+				it++;
+				i++;
+			}
+
+		}
+		return it;
+	}
+
+	/*
+	*	Giving two sets of sizes N and M,  this function will redefine them
+	*	so that their sizes are T or T-1 
+	*/
+	inline
+	void balance_halfs( mset_long& mysetl, mset_long& mysetr )
+	{
+	 
+		if ( mysetl.size() == mysetr.size() || abs( mysetl.size()-mysetr.size() ) == 1  )
+			return; 
+		   
+		if ( mysetl.size() < mysetr.size() )
+		{
+			ll tmp = *mysetr.begin();
+			mysetr.erase( mysetr.begin() );
+			mysetl.emplace( tmp );
+			
+		}
+		else if ( mysetl.size() > mysetr.size() )
+		{
+			ll tmp = *prev( mysetl.end() );
+			mysetl.erase( prev( mysetl.end()) );
+			mysetr.emplace( tmp );
+		}
+
+		balance_halfs(mysetl,mysetr);
+			
+	}
+	
+	// Implementation of search for inversions from the book
+	// algorithm  illuminated using brute force
+	//  given 1 4 2, an inversion is the pair 4 and 2 because
+	// 4 > 2 but the indexes of them in the array are index1< index2
+	// Complexity O(n)
+	inline
+	int num_inverstions_brute( const vector<int>& _arr )
+	{
+		int invertions = 0;
+		int n = _arr.size();
+		for( int i = 0; i < n; i++ )
+		{
+			for( int j = i+1;  j < n-1; j++)
+				if ( _arr[i] > _arr[j] ) invertions++;
+		}
+		return invertions;
+	}
+	
+	// supports genertic types
+	template<typename T>
+	inline
+	int num_inverstions_brute( const vector<T>& _arr )
+	{
+		int invertions = 0;
+		int n = _arr.size();
+		for( int i = 0; i < n; i++ )
+		{
+			for( int j = i+1;  j < n-1; j++)
+				if ( _arr[i] > _arr[j] ) invertions++;
+		}
+		return invertions;
+	}
+	
+	
+	inline
+	vector<int> split_vector( const vector<int>& _arr, int _left_right)
+	{
+		int n = _arr.size();
+		
+		int lfirst = 0;
+		int llast = 0;
+		int rfirst = 0;
+		int rlast = n-1;
+		
+		if ( _arr.size()%2 == 0 )
+		{
+			llast = (n/2) -1;
+			rfirst = n/2;			
+		}
+		else
+		{
+			llast  = (n-1)/2;
+			rfirst = (n+1)/2;
+		}
+		if ( _left_right == 0 )
+		{
+			vector<int> A{  _arr.begin()+lfirst, _arr.begin()+ llast};
+			return A;
+		}
+		vector<int> B{  _arr.begin()+rfirst, _arr.begin()+ rlast  };
+		return B;
+	}
+
+	// Implementation of search for inversions from the book
+	// algorithm  illuminated using the divide and conquer strategy
+	//  given 1 4 2, an inversion is the pair 4 and 2 because
+	// 4 > 2 but the indexes of them in the array are index1< index2
+	// Complexity O(nlong(n))	
+	inline
+	int num_inverstions( const vector<int>& _arr )
+	{
+		vector<int> A = split_vector( _arr, 0 );
+		vector<int> B = split_vector( _arr, 1 );
+		return 0;
+		
+	}
+	
+	// starts computing the execution time of a code
+	inline
+	high_resolution_clock::time_point start_tracking()
+	{
+		return high_resolution_clock::now(); 
+	}
+
+	// stops  computing the execution time of a code and print 
+	// elapsed time
+	inline
+	void end_tracking( high_resolution_clock::time_point& start, const char* msg )
+	{
+		cout << "________________________________________" << endl;
+		cout << msg << endl;
+		auto stop = high_resolution_clock::now(); 
+		auto duration = duration_cast<microseconds>(stop - start);    
+		cout << "Execution timing:" << duration.count() << endl; 
+		cout << "________________________________________" << endl;
+	}
+}
+
+#endif
