4bc1d6a0a5a6f38b375d6880b1490a524657fcde
zlogdanbr <54685402+zlogdanbr@users.noreply.github.com>
2020-05-25 17:53:23 -0300

    replaced matrix library for a better one
    Engein is very cumbersome so I am using this more sucint and clear
    https://github.com/andrea993/Matrix-Library-Cpp

--- src/ml.cpp
+++ src/ml.cpp
@@ -1,36 +1,131 @@
 #include "util.h"
-#include "eigen.h"
 
-void eigen_test()
+// Using this library https://github.com/andrea993/Matrix-Library-Cpp
+#include "mat/Matrix.h"
+// ------------------------------------------------------------------
+
+using namespace mytools::util;
+
+template<typename T>
+Matrix<T> transpose( const Matrix<T>& a  )
 {
-    // 4 samples 3x4
+	unsigned int rows = a.Rows();
+	unsigned int cols = a.Cols();
+	
 
-    vector<vector<double>>  dm1{  { 1.1, 1.2, 1.3, 1.4 },
-                                { 2.1, 2.2, 2.3, 2.4 },
-                                { 3.1, 3.2, 3.3, 3.4 }};
-    // 3 outputs    
-
-    vector<vector<double>>  dm2{ { 4.1, 4.2, 4.3 } };     
-    // 4 coeficents
-    vector<vector<double>>  dm3{ { 0, 0, 0, 0 } };  
-    
-    MatrixXf x = convertVector2Matrix4d(dm1, 4);
-    MatrixXf y = convertVector2Matrix4d(dm2, 3);
-    MatrixXf coef = convertVector2Matrix4d(dm3, 4);
-    int m = 4;
-    double zeta = 0.001;
-    MatrixXf coefn = coef.transpose() - zeta*GradientDescent(x,y,coef,m);
-    coef = coefn.transpose();
-    
-	for(int i = 0; i < 10 ; i++ )
-    {
-        coefn = coef.transpose() - zeta*GradientDescent(x,y,coef,m);
-        coef = coefn.transpose();
-    }
-    std::cout << coefn << std::endl;
+	Matrix<T> transposed(cols, rows, 0);
+	
+	for(unsigned int i = 0; i < rows; i++ )
+	{
+		for( unsigned int j = 0; j < cols ; j++ )
+		{
+			transposed[j][i] = a[i][j];
+		}
+	}
+	return transposed;
 }
+
+void times( Matrix<double>& a , int ct )
+{
+	for(unsigned int i = 0; i < a.Rows(); i++ )
+	{
+		for( unsigned int j = 0; j < a.Cols() ; j++ )
+		{
+			a[i][j] =ct * a[i][j];
+		}
+	}
+}
+
+Matrix<double> calculate_gradient( Matrix<double>& x, Matrix<double>& y, Matrix<double>& coef, int m)
+{
+	Matrix<double> t = transpose<double>(x)*(x*coef - y);
+	times( t, m );
+	return t;
+}
+
+Matrix<double> convertVector2Matrix( vector<vector<double>>& data )
+{
+	
+	unsigned int N = data.size();
+	unsigned int M = data[0].size()-1;
+	
+	Matrix<double> t{N,M};
+	
+	for(  unsigned int i = 0; i < N ; i++  )
+	{
+		for(  unsigned int j = 0; j < M ; j++  )
+		{
+			t[i][j] = data[i][j];
+		}
+	}
+	
+	return t;
+}
+
+Matrix<double> convertcol2Matrix( vector<double>& y )
+{
+	
+	unsigned int N = y.size();
+	
+	Matrix<double> t{N,1};
+	
+	for(  unsigned int i = 0; i < N ; i++  )
+	{
+		t[i][0] = y[i];
+	}
+	return t;
+}
+ 
+void pr( const vector<double>& y )
+{
+	for( const auto& v: y )
+	{
+		cout << v << " " ;
+	}
+	cout << "\n";
+}
+
+void test1()
+{
+	// two samples as they were read from csv
+	Matrix<double> x{ 	{1, 2, 3},
+						{4, 5, 6} };	
+	
+	// two outputs as they are read as a collumn from csv
+	Matrix<double> yt = {{ 7, 8 }};
+	Matrix<double> y = transpose( yt );
+	
+	// coef, we have 3 variables so we need 4 coeficients	
+	Matrix<double> coefy= {{ 0, 0, 0 }};
+	Matrix<double> coef = transpose( coefy);
+
+	
+	cout << calculate_gradient( x, y, coef, 1 ) << "\n";
+}
+
+void test2()
+{
+	csvprocessing csv{"test.csv"};
+	vector<vector<double>> data;
+	vector<double> yr;
+	
+	csv.readCSV( data, 4, false );	
+	Matrix<double> x  = convertVector2Matrix( data );	
+	csv.getcol( data, yr, 3 );
+	
+	Matrix<double> y  = convertcol2Matrix( yr );
+	
+	// coef, we have 3 variables so we need 4 coeficients	
+	Matrix<double> coefy= {{ 0, 0, 0 }};
+	Matrix<double> coef = transpose( coefy);
+
+	
+	cout << calculate_gradient( x, y, coef, 2 ) << "\n";
+}
+
 
 int main()
 {
-	eigen_test();
+	test2();
+
 }
